d3f103e06cfd5846f326e3376abd874dbddaf897
ab-n3xt <46637527+ab-n3xt@users.noreply.github.com>
2024-03-14 07:21:17 -0400

    Job titles now animate

--- script.js
+++ script.js
@@ -23,6 +23,49 @@ async function loadHTML(name) {
 }
 
 /*== Animated subtitle ==*/
-var jobTitles = ["Programmer", "Digital Marketer"];
 var index = 0;
+var delay = 0.1;
+const delayRate = 0.05;
 var animatedJobTitle = document.querySelector(".animatedJobTitle");
+
+const jobTitles = ["Programmer", "Digital Marketer"];
+const spannedJobTitles = []
+for(const jobTitle of jobTitles) {
+    var newJobTitle = new String();
+    for(const letter of jobTitle) {
+        var newLetter = new String();
+        if (letter === ' ') newLetter = `<span style='animation-delay: ${delay}s' class='letter'>&nbsp</span>` ;
+        else newLetter = `<span style='animation-delay: ${delay}s' class='letter'>${letter}</span>`;
+        newJobTitle = newJobTitle.concat(newLetter);
+        delay += delayRate;
+    }
+    spannedJobTitles.push(newJobTitle);
+}
+
+animatedJobTitle.innerHTML = spannedJobTitles[index];
+const letters = document.querySelectorAll(".letter");
+const lastLetter = letters[letters.length-1];
+lastLetter.addEventListener("animationend", () => {
+    index += 1;
+    if(index >= spannedJobTitles.length) index = 0;
+    animatedJobTitle.innerHTML = spannedJobTitles[index];
+})
+
+// identify an element to observe
+var elementToObserve = window.document.getElementById('animatedJobTitle');
+
+// create a new instance of 'MutationObserver' named 'observer', 
+// passing it a callback function
+var observer = new MutationObserver(function(mutationsList, observer) {
+    const letters = document.querySelectorAll(".letter");
+    const lastLetter = letters[letters.length-1];
+    lastLetter.addEventListener("animationend", () => {
+        index += 1;
+        if(index >= spannedJobTitles.length) index = 0;
+        animatedJobTitle.innerHTML = spannedJobTitles[index];
+    })
+});
+
+// call 'observe' on that MutationObserver instance, 
+// passing it the element to observe, and the options object
+observer.observe(elementToObserve, {childList: true});
