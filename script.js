efd3a9e3562008e414399c357f43aab59672e297
Aidan B <aidan.buchanan@gmail.com>
2024-03-20 06:43:32 -0400

    Adjusted content

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 const DEBUG = true;
-/*== SPA Functionality ==*/
-const headerNavLinks = document.getElementsByTagName('nav')[0].children;
+/*== Animated hoverbar ==*/
 
-for (var i = 0; i < headerNavLinks.length; i++) {
-    (function () {
-        const headerNavLink = headerNavLinks[i];
-        const headerNavLinkName = headerNavLink.getAttribute('name');
-        headerNavLinks[i].addEventListener("click", () => {
-            loadHTML(headerNavLinkName);
-        })
-    }());
-}
-async function fetchHtmlAsText(url) {
-    return await (await fetch(url)).text();
-}
-
-async function loadHTML(name) {
-    const nameOfFile = name + ".html";
-    if (DEBUG) console.log("Loading: " + nameOfFile + "...");
-    const contentDiv = document.getElementById("content");
-    contentDiv.innerHTML = await fetchHtmlAsText(nameOfFile);
-}
 
 /*== Animated subtitle ==*/
 var index = 0;
@@ -28,7 +8,7 @@ var delay = 0.1;
 const delayRate = 0.05;
 var animatedJobTitle = document.querySelector(".animatedJobTitle");
 
-const jobTitles = ["Programmer", "Digital Marketer", "E-Commerce"];
+const jobTitles = ["Digital Marketing", "E-Commerce", "Web Development"];
 const spannedJobTitles = []
 for(const jobTitle of jobTitles) {
     var newJobTitle = new String();
